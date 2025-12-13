ba5be698f98942641529a8d474dd8d9485d1a506
samg1010 <samgaspar1010@gmail.com>
2025-12-14 00:57:39 +0800

    Add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+// src/app/login/page.tsx
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Login Page</h1>
+    </div>
+  )
+}
